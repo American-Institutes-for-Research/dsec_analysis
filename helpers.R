79509324937ad5de6116a8792459ee77345eeb3c
Nisha <npatel@.air.org>
2019-07-21 16:32:09 -0400

    function-ized graph creation

--- helpers.R
+++ helpers.R
@@ -11,3 +11,45 @@ install_pkgs = function(pkg){
   library(pkg, character.only = TRUE)
   
 } # end install_pkgs()
+
+
+# REQUIRES: df, y_var, fill_var, text_labels needed, plot_labels vector (fill & plot title), color palette)
+# MODIFIES: nothing
+# EFFECTS: makes pie graph of data
+make_pie_graph <- function(df, y_var, fill_var, text_labels, plot_labs,clr_palette){
+  df %>% ggplot(mapping=aes(x = '', y=y_var, fill=fill_var)) +
+    geom_bar(stat="identity") +
+    coord_polar(theta = 'y', start = 0) +
+    geom_text(mapping=aes(label=text_labels,fontface="bold"), position = position_stack(vjust = 0.5)) +
+    labs(x = NULL, y = NULL, fill = plot_labs[1], title = plot_labs[2]) +
+    theme_classic() +
+    theme(axis.text = element_blank(),
+          axis.line = element_blank(),
+          legend.title = element_text(face="bold"),
+          legend.text = element_text(face="bold"),
+          plot.title = element_text(face="bold",hjust=0.5, vjust = -10)
+    ) +
+    scale_fill_manual(values=c(clr_palette))
+  
+}
+
+# REQUIRES: df, x_var, y_var, plot_labels vector (x,y, & plot title), color palette)
+# MODIFIES: nothing
+# EFFECTS: makes bar graph of counts data
+make_bar_graph_counts <- function(df, x_var, y_var, plot_labs,clr_palette){
+  
+  df %>% ggplot(mapping=aes(x = x_var, y=y_var,fill=x_var)) +
+  geom_bar(stat="identity", position = "dodge", show.legend = FALSE) +
+  geom_text(mapping=aes(label=y_var,fontface="bold",vjust=-0.5)) +
+  labs(x = plot_labs[1], y = plot_labs[2], title = plot_labs[3]) +
+  theme_classic() +
+  theme(
+    plot.title = element_text(face="bold",hjust=0.5),
+    axis.text.x = element_text(face="bold",color="black"),
+    axis.text.y = element_text(face="bold",color="black"),
+    axis.title.x = element_text(face="bold",color="black"),
+    axis.title.y = element_text(face="bold",color="black")
+  ) +
+  scale_fill_manual(values=c(clr_palette))
+
+}
--- needs_sensing_graphs.R
+++ needs_sensing_graphs.R
@@ -8,62 +8,50 @@ RColorBrewer::display.brewer.all()
 spectral_palette = brewer.pal(11, "Spectral")
 color_palette = spectral_palette[c(3,8,10)]
 
-# tab names
+# tab names and partner names
 tab_names = c("Structure", "Content", "Participants","Geographic Outreach","Outcomes", "Logistics&Timing")
 partner_names = c("CEE","FIRST","Math Counts","NCWIT","NMSI","SSP","TGR",
                   "TIES","USASEF","Dayton STEM Center", "Morgan State CEMSE", "UC San Diego CREATE")
+
+
 # start with structure sheet
 structure = read_excel(str_c(input_fp, 'Taxonomy and Snapshot Coding.xlsx'),sheet = tab_names[1],skip = 2)
 
-
-####### ENGAGEMENT TYPE ###### 
+####### ENGAGEMENT TYPE - FIGURE 1 ###### 
 engagement_type = structure[,c('X__1','ENVIRONMENT TYPE','X__2','X__3')]
 colnames(engagement_type) <- c("program",'in_school','out_of_school','adult_learning')
-
 
 idx_to_drop = c(1, which(is.na(engagement_type$program)), which(engagement_type$program %in% partner_names))
 engagement_type = engagement_type[-unique(idx_to_drop),]
 engagement_type[is.na(engagement_type)] <- 0
 
-tot_programs = nrow(engagement_type)
+tot_programs <- nrow(engagement_type)
 print(str_c("Total estimated programs: ", nrow(engagement_type)))
 
 # reshape for groups
-et_reshape = gather(engagement_type, key = "engagement_type", value = "value",colnames(engagement_type)[!colnames(engagement_type) %in% c("program")])
-et_final = et_reshape %>% filter(value==1) %>% group_by(engagement_type) %>% summarize(n=n()) %>% mutate(prop_n = n/sum(n))
-
+et_reshape <- gather(engagement_type, key = "engagement_type", value = "value",colnames(engagement_type)[!colnames(engagement_type) %in% c("program")])
+et_final <- et_reshape %>% filter(value==1) %>% group_by(engagement_type) %>% summarize(n=n()) %>% mutate(prop_n = n/sum(n))
 et_final$engagement_type[which(et_final$engagement_type == "adult_learning")] <- "Adult Learning"
 et_final$engagement_type[which(et_final$engagement_type == "in_school")] <- "In School"
 et_final$engagement_type[which(et_final$engagement_type == "out_of_school")] <- "Out of School"
 
-
-et_pie <- et_final %>% ggplot(mapping=aes(x = '', y=prop_n, fill=engagement_type)) +
-  geom_bar(stat="identity") +
-  coord_polar(theta = 'y', start = 0) +
-  geom_text(mapping=aes(label=str_c(round(prop_n*100),"%"),fontface="bold"), position = position_stack(vjust = 0.5)) +
-  labs(x = NULL, y = NULL, fill = "Engagement Type", title = "Engagement Type of DSEC Partner Programs") +
-  theme_classic() +
-  theme(axis.text = element_blank(),
-        axis.line = element_blank(),
-        legend.title = element_text(face="bold"),
-        legend.text = element_text(face="bold"),
-        plot.title = element_text(face="bold",hjust=0.5, vjust = -10)
-        ) +
-  scale_fill_manual(values=c(color_palette))
-
+# percentage pie plot
+et_text_labs <- str_c(round(et_final$prop_n*100),"%")
+et_pie_plot_labs <- c("Engagement Type","Engagement Type of DSEC Partner Programs")
+et_pie <- make_pie_graph(et_final, et_final$prop_n, et_final$engagement_type, et_text_labs , et_pie_plot_labs ,color_palette)
 et_pie 
 
-et_bar_counts <- et_final %>% ggplot(mapping=aes(x = engagement_type, y=n,fill=engagement_type)) +
-  geom_bar(stat="identity", position = "dodge", show.legend = FALSE) +
-  geom_text(mapping=aes(label=n,fontface="bold",vjust=-0.5)) +
-  labs(x = 'Engagement Type', y = 'Number of Programs', title = "Engagement Type of DSEC Partner Programs") +
-  theme_classic() +
-  theme(
-        plot.title = element_text(face="bold",hjust=0.5),
-        axis.text.x = element_text(face="bold"),
-        axis.text.y = element_text(face="bold")
-  ) +
-  scale_fill_manual(values=c(color_palette))
+# count pie plot
+et_text_labs <- et_final$n
+et_pie_plot_labs <- c("Engagement Type","Engagement Type of DSEC Partner Programs")
+et_pie_count <- make_pie_graph(et_final, et_final$n, et_final$engagement_type, et_text_labs , et_pie_plot_labs ,color_palette)
+et_pie_count 
 
-et_bar_counts
+# count bar plot
+et_bar_plot_labs <- c( 'Engagement Type', 'Number of Programs', "Engagement Type of DSEC Partner Programs")
+et_barplot <- make_bar_graph_counts(et_final, et_final$engagement_type, y=et_final$n, et_bar_plot_labs,color_palette)
+et_barplot
+
+
+
 
